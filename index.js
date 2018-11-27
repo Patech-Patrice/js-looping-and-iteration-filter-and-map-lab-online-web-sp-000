7ad900a3c2b98a5b94f8131406e2f249d1058c5f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-27 19:32:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 // Code your solution here:
-function driversWithRevenueOver(drivers,revenue){
-  return drivers.filter(revenue => revenue. 
-  
-  
-}
+
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) { return driver.revenue > revenue });
 }
-
-
-
-
-
-
-
-
-
 
 
 
